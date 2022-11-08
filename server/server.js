93e0bc769399376e563be7ec05c492a9d8a8057a
ahmedraed777 <araed2217@gmail.com>
2022-11-08 14:33:02 +0200

    express server added

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+const port = 8080
+
+const publicPath = path.join(__dirname, '..', 'public')
+
+app.use(express.static(publicPath))
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'))
+})
+
+app.listen(port, () => {
+    console.log(`the server is running at http://localhost:${port}`)
+})
