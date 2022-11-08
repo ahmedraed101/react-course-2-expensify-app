568ccae176b508ba7b46eababf339e9da66cedd9
ahmedraed777 <araed2217@gmail.com>
2022-11-08 15:40:53 +0200

    setup production build and server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-const port = 8080
+const port = process.env.PORT || 3002
 
 const publicPath = path.join(__dirname, '..', 'public')
 
